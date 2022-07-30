341458c6852e39e27750ffff80a90faab3067ac0
Yakumo Saki <yakumo@ziomatrix.org>
2022-07-30 16:34:44 +0900

    clean up

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -21,13 +21,12 @@ func Initiallize() {
 		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
 	}
 
-	zerolog.TimeFieldFormat = time.RFC3339Nano
-	outputWithTimestamp := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "2006-01-02 15:04:05.000"}
-	outputNoTimestamp := zerolog.ConsoleWriter{Out: os.Stdout}
-
 	if config.LOG_NO_DATETIME {
+		outputNoTimestamp := zerolog.ConsoleWriter{Out: os.Stdout}
 		log.Logger = zerolog.New(outputNoTimestamp).With().Caller().Logger()
 	} else {
+		zerolog.TimeFieldFormat = time.RFC3339Nano
+		outputWithTimestamp := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "2006-01-02 15:04:05.000"}
 		log.Logger = zerolog.New(outputWithTimestamp).With().Timestamp().Caller().Logger()
 	}
 }
